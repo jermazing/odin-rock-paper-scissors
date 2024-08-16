6b42c09b6ce735aa56baef444847c7304adb49ea
jermazing <57467514+jermazing@users.noreply.github.com>
2024-08-15 17:50:32 -0700

    ADD FUNCTIONS TO MAIN.JS

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 console.log("Hello World2")
 
 
+let getComputerChoice = function() {
+    let rando = Math.floor(Math.random() * 3)   + 1;
+
+    if (rando === 1) {
+        return "Rock"
+    } else if (rando === 2) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+
+}
+
+console.log(getComputerChoice())
+
+let getHumanChoice = function(pls) {
+    
+}
+
+
