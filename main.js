0eb90cc41bfa724b014a2e2a29b68bfd37e2e5b3
jermazing <57467514+jermazing@users.noreply.github.com>
2024-08-22 16:32:04 -0700

    Add result calculator function

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ console.log("Hello World2")
 
 
 let getComputerChoice = function() {
-    let rando = Math.floor(Math.random() * 3)   + 1;
+    let cpuChoice = Math.floor(Math.random() * 3)   + 1;
 
-    if (rando === 1) {
+    if (cpuChoice === 1) {
         return "Rock"
-    } else if (rando === 2) {
+    } else if (cpuChoice === 2) {
         return "Paper"
     } else {
         return "Scissors"
@@ -16,8 +16,30 @@ let getComputerChoice = function() {
 
 console.log(getComputerChoice())
 
-let getHumanChoice = function(pls) {
+let getHumanChoice = function(myChoice) {
     
 }
 
 
+if (myChoice === cpuChoice) {
+    return "TIE!"
+} else if (myChoice === "Rock") {
+    if (cpuChoice === "Scissors") {
+        return "You chose ${myChoice}! Computer chose ${cpuChoice}! You Win!"
+    } else {
+        return "You chose ${myChoice}! Computer chose ${cpuChoice}! You Lose!"
+    }
+} else if (myChoice === "Paper") {
+    if (cpuChoice === "Rock") {
+        return "You chose ${myChoice}! Computer chose ${cpuChoice}! You Win!"
+    } else {
+        return "You chose ${myChoice}! Computer chose ${cpuChoice}! You Lose!"
+    }
+
+} else {
+    if (cpuChoice === "Paper") {
+        return "You chose ${myChoice}! Computer chose ${cpuChoice}! You Win!"
+    } else {
+        return "You chose ${myChoice}! Computer chose ${cpuChoice}! You Lose!"
+    }
+}
